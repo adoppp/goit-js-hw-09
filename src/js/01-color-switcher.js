e8677627b140ada3442ce50837c59bbd9579d36f
adoppp <121878757+adoppp@users.noreply.github.com>
2023-06-11 22:04:53 +0200

    ss

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+console.log(startBtn);
+const endBtn = document.querySelector('[data-stop]');
+console.log(endBtn);
+const body = document.body;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let interval = null;
+
+function bodyColor() { 
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+};
+
+startBtn.addEventListener('click', () => { 
+    interval = setInterval(bodyColor, 1000)
+    startBtn.setAttribute('disabled', 'true');
+    endBtn.removeAttribute('disabled');
+});
+
+
+endBtn.addEventListener('click', () => {
+    clearInterval(interval);
+    endBtn.setAttribute('disabled', 'true');
+    startBtn.removeAttribute('disabled');
+});
